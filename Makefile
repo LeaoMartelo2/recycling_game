2302ddc02de7fea3d74ade009ee86c08f73f2802
LeaoMartelo <leaomartelo@gmail.com>
2024-10-19 23:54:15 -0300

    stuff

--- Makefile
+++ Makefile
@@ -2,8 +2,9 @@ CC = gcc
 #link raylib statically, GL does not have a static lib, cant use -static 
 #atleast the final user wont need to already have raylib's .so
 RAY_FLAGS = -lGL -lm -lpthread -ldl -lrt -lX11 -L./raylib/ -lraylib  
+FLAGS = -Wall -Wextra -O2 -dbg
 
 
 update:
 	${CC} src/main.c ${RAY_FLAGS} -o main
-	./main
+	./main -debug
--- src/main.c
+++ src/main.c
@@ -1,15 +1,15 @@
-#include "raylib.h"
+#include <raylib.h>
 #include <string.h>
 
 #define WINDOW_SIZE_FACTOR 60.0f
 
 int main(int argc, char **argv) {
 
-    bool debug = false;
+    bool debug_mode = false;
 
     for (int i = 0; i < argc; i++) {
         if (strcmp(argv[i], "-debug") == 0) {
-            debug = true;
+            debug_mode = true;
         }
     }
 
@@ -23,7 +23,7 @@ int main(int argc, char **argv) {
         float screen_width = GetScreenWidth();
         float screen_height = GetScreenHeight();
 
-        Rectangle border = {
+        Rectangle screen_border = {
             .x = 0,
             .y = 0,
             .width = screen_width,
@@ -32,9 +32,11 @@ int main(int argc, char **argv) {
 
         BeginDrawing();
         {
-            DrawFPS(0, 0);
-
-            DrawRectangleLines(0, 0, screen_width, screen_height, RED);
+            if (debug_mode) {
+                DrawFPS(2, 2);
+                DrawRectangleLines(0, 0, screen_width, screen_height, RED);
+                DrawRectangleLinesEx(screen_border, 2, RED);
+            }
         }
         EndDrawing();
     }
