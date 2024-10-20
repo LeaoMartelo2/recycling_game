50e315eec22909f18d36ba5202f70d8d905a3e19
LeaoMartelo <leaomartelo@gmail.com>
2024-10-20 19:20:24 -0300

    added sprite rotate to cursor logic

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
+#include <math.h>
 #include <raylib.h>
+#include <stdio.h>
 #include <string.h>
 
-#define WINDOW_SIZE_FACTOR 60.0f
+/*#include "assets/sprites/player/player_idle.raw"*/
+
+#define WINDOW_SIZE_FACTOR 75.0f
 
 int main(int argc, char **argv) {
 
@@ -17,26 +21,52 @@ int main(int argc, char **argv) {
     InitWindow(16 * WINDOW_SIZE_FACTOR, 9 * WINDOW_SIZE_FACTOR, "Recycling Game");
     SetTargetFPS(60);
 
+    /*Image player_img = {*/
+    /*    .data = PLAYER_EXPORT_DATA,*/
+    /*    .width = PLAYER_EXPORT_WIDTH,*/
+    /*    .height = PLAYER_EXPORT_HEIGHT,*/
+    /*    .format = PLAYER_EXPORT_FORMAT,*/
+    /*    .mipmaps = 1,*/
+    /*};*/
+
     while (!WindowShouldClose()) {
 
         ClearBackground(BLACK);
         float screen_width = GetScreenWidth();
         float screen_height = GetScreenHeight();
 
-        Rectangle screen_border = {
-            .x = 0,
-            .y = 0,
-            .width = screen_width,
-            .height = screen_height,
-        };
+        Vector2 mouse_pos = GetMousePosition();
+
+        Texture2D player = LoadTexture("src/assets/sprites/player/idle_cut.png");
+
+        Vector2 texture_position = {screen_width / 2, screen_height / 2};
+
+        Vector2 origin = {player.width / 2.0f, player.height / 2.0f};
+
+        float angle = atan2(mouse_pos.y - texture_position.y, mouse_pos.x - texture_position.x) * (180.0f / PI);
+
+        float player_scale = 0.2f;
 
         BeginDrawing();
         {
             if (debug_mode) {
                 DrawFPS(2, 2);
-                DrawRectangleLines(0, 0, screen_width, screen_height, RED);
-                DrawRectangleLinesEx(screen_border, 2, RED);
+                printf("x: %f y:%f a:%f \n", mouse_pos.x, mouse_pos.y, angle);
             }
+
+            DrawTexturePro(
+                player,
+                (Rectangle){0, 0, player.width, player.height},
+                (Rectangle){
+                    texture_position.x,
+                    texture_position.y,
+                    player.width * player_scale,
+                    player.height * player_scale},
+                (Vector2){
+                    origin.x * player_scale,
+                    origin.y * player_scale},
+                angle,
+                WHITE);
         }
         EndDrawing();
     }
